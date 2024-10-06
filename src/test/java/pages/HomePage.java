47e48ba11167d3bfb71fa62b7bc8c6138b872e6f
Aatif65 <asauban65@gmail.com>
2024-10-06 11:39:19 +0530

    added page clas for HomePage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,26 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
 
+	private WebDriver driver=null;
+	
+	@FindBy (xpath="//*[@title='Search']")
+	private WebElement  searchBox;
+	
+	public HomePage(WebDriver driver)
+	{
+		PageFactory.initElements(driver,this );
+		this.driver=driver;
+	}
+	
+ 
+	public void fillSearchBox(String x)
+	{
+		searchBox.sendKeys(x);
+	}
 }
