f90ec69b3ca7792ad7124103585258816a6b5340
Aatif65 <asauban65@gmail.com>
2024-10-06 11:52:12 +0530

    added wait conditions

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,23 +4,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 
 public class HomePage {
 
 	private WebDriver driver=null;
+	private FluentWait<WebDriver> wait=null;
 	
 	@FindBy (xpath="//*[@title='Search']")
 	private WebElement  searchBox;
 	
-	public HomePage(WebDriver driver)
+	public HomePage(WebDriver driver,FluentWait<WebDriver> wait)
 	{
-		PageFactory.initElements(driver,this );
+		PageFactory.initElements(driver,this);
 		this.driver=driver;
+		this.wait=wait;
+		
 	}
 	
  
 	public void fillSearchBox(String x)
 	{
-		searchBox.sendKeys(x);
+		wait.until(ExpectedConditions.visibilityOf(searchBox)).sendKeys(x);		
 	}
 }
