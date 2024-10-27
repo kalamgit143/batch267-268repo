4f4517e9b934ebe9e0d26f8384eb6e01d42fdbfe
kalamgit143 <66198415+kalamgit143@users.noreply.github.com>
2024-10-27 08:47:31 +0530

    Update Program1.java

--- src/test/java/tests/Program1.java
+++ src/test/java/tests/Program1.java
@@ -11,6 +11,7 @@ public class Program1
 	@Test
 	public void method()
 	{
+		//Sort the given list of strings using java streams
 		List<String> names=Arrays.asList("kalam","bros","steave","sisters","ratan","students");
 		List<String> result=names.stream().sorted().collect(Collectors.toList());
         System.out.println("Output: "+result);
